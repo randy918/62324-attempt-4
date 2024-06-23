0e0c036fefc78fa69cb61029290de68ee1e571b1
Randy Miller <randy918@gmail.com>
2024-06-23 14:37:35 -0500

    6-23-24-1437

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const images = document.querySelectorAll('#carousel img');
+const dots = document.querySelector('#dots');
+const desc = document.querySelector('#desc');
+
+let speed = 7500;
+let index = 0;
+let intervalID;
+
+images.forEach((image, i) => {
+
+    const span = document.createElement('span');
+    span.className = 'dot';
+    if (i === 0) span.classList.add('active');
+
+    span.addEventListener('click', () => {
+        index = 1;
+        startInterval();
+        updateContent();
+
+    });
+    dots.appendChild(span);
+
+})
+
+startInterval();
+
+function startInterval() {
+
+    if (intervalID) clearInterval(intervalID);
+
+    intervalID = setInterval(() => {
+        index++;
+        if (index === images.length) index = 0;
+        updateContent();
+    }, speed);
+
+}
+
+updateContent();
+
+function updateContent() {
+
+    images.forEach(item => item.classList.remove('active'));
+    images[index].classList.add('active');
+
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach(item => item.classList.remove('active'));
+    dots[index].classList.add('active');
+
+}
